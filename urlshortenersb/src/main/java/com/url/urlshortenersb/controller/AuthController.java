e734b44e46ba407ad21f313110c9ea25dda8117d
Harsh <taruharsh3093@gmail.com>
2025-06-07 00:15:27 +0530

    Add AuthController and user registration functionality

---
+++ urlshortenersb/src/main/java/com/url/urlshortenersb/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.url.urlshortenersb.controller;
+
+import com.url.urlshortenersb.dtos.RegisterRequest;
+import com.url.urlshortenersb.models.User;
+import com.url.urlshortenersb.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/public/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
+        User user = new User();
+        user.setEmail(registerRequest.getEmail());
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(registerRequest.getPassword());
+        user.setRole("ROLE_USER");
+        userService.registerUser(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
---
+++ urlshortenersb/src/main/java/com/url/urlshortenersb/service/UserService.java
@@ -0,0 +1,19 @@
+package com.url.urlshortenersb.service;
+import com.url.urlshortenersb.models.User;
+import com.url.urlshortenersb.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
